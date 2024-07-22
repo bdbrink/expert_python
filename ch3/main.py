adfec700aab889a5fca13e3ab1c8b39f9b9d0882
bdbrink <brendan.brink@outlook.com>
2024-07-22 17:30:30 -0500

    adding operators to function for dicts

--- ch3/main.py
+++ ch3/main.py
@@ -10,4 +10,33 @@ def merge_update():
     value += (4,5,6)
     print(value)
 
+print("#### Merge Update ###")
 merge_update()
+print("#### Merge Update ###")
+
+def binary_operators():
+    # intersection &
+    # union |
+    # difference -
+    # symmetric ^
+    
+    ex1 = {1,2,3} & {1,4}
+    print(ex1)
+    
+    ex2 = {1,2,3} | {1,4}
+    print(ex2)
+    
+    ex3 = {1,2,3} - {1,4}
+    print(ex3)
+    
+    ex4 = {1,2,3} ^ {1,4}
+    print(ex4)
+    
+    ex5 = {"a": 1} | {"a": 3, "b": 2}
+    print(ex5)
+
+print("\n")
+print("#### binary operators ###")
+binary_operators()
+print("#### binary operators ###")
+print("\n")
