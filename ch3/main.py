0d98491879f741a7081f1fb7f7e59b124163f301
bdbrink <brendan.brink@outlook.com>
2024-07-22 17:22:58 -0500

    adding merge function

---
+++ ch3/main.py
@@ -0,0 +1,13 @@
+
+def merge_update():
+    list1 = [1,2,3]
+    list2 = [4,5,6]
+    
+    merge_list = list1 + list2
+    print(merge_list)
+    
+    value = (1,2,3)
+    value += (4,5,6)
+    print(value)
+
+merge_update()
