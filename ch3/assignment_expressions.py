a7216654177f0ffc4e651f12189c584be3834545
bdbrink <brendan.brink@outlook.com>
2024-07-23 18:12:43 -0500

    adding assignment expressions

---
+++ ch3/assignment_expressions.py
@@ -0,0 +1,14 @@
+
+def syntax_feats():
+    lambda x: x**2
+    
+    type("MyClass", (), {})
+    
+    squares_of_2 = [x**2 for x in range(10)]
+    
+    if number % 2 == 0:
+        print("odd")
+    else:
+        print("even")
+    
+syntax_feats()
