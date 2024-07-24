76e6185db86f16bc99353c141b14b9eeb4263444
bdbrink <brendan.brink@outlook.com>
2024-07-24 18:09:37 -0500

    adding comment

--- ch3/type_hint.py
+++ ch3/type_hint.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+# telling function d needs to be dictionary and key needs to be string
 def get_ci(d: dict, key: str) -> Any:
     for k,v in d.items():
         if key.lower() == k.lower():
