03320223da36751684f42563b6e9d47d7fe17e05
bdbrink <brendan.brink@outlook.com>
2024-07-29 18:08:06 -0500

    adding point with and without match args

--- ch3/where_is_point.py
+++ ch3/where_is_point.py
@@ -1,3 +1,4 @@
+## need to specify x and y without match args
 class Point:
     x: int
     y: int
@@ -20,7 +21,37 @@ def where_is_point(point):
             print("Not a point")
 
 # Create an instance of Point
-point = Point(0, 0)  # Or any other x and y values you want
+point = Point(0, 1)  # Or any other x and y values you want
 
 # Call the function with the Point instance
 where_is_point(point=point)
+
+## In this example no need to specify x and y as the match args does that
+class Point2:
+    x: int
+    y: int
+    
+    __match_args__ = ("x", "y")
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+def where_is_point2(point2):
+    match point2:
+        case Point2(0, 0):  # Using positional syntax
+            print("Origin")
+        case Point2(0, y):
+            print(f"Y={y}")
+        case Point2(x, 0):
+            print(f"X={x}")
+        case Point2():
+            print("Somewhere else")
+        case _:
+            print("Not a point")
+
+# Create an instance of Point2
+point2 = Point2(0, 1) 
+
+# Call the function with the Point2 instance
+where_is_point2(point2)
