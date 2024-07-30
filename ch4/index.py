324fd6eeab46f023137aa93049b0f190f9ba904a
bdbrink <brendan.brink@outlook.com>
2024-07-30 17:22:31 -0500

    adding iteration over list

---
+++ ch4/index.py
@@ -0,0 +1,10 @@
+
+# less pythonic
+list = [1, 2, 3, 4, 5, 6]
+for index in range(len(list)):
+    print(list[index])
+    
+## More pythonic
+list2 = [1, 2, 3, 4, 5, 6]
+for item in list2:
+    print(item)
