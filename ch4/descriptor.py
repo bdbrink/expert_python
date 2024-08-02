bbdd3ebac605adfce8088bdd41b7cb0eb180791e
bdbrink <brendan.brink@outlook.com>
2024-08-02 14:51:54 -0500

    fixing class return for object

--- ch4/descriptor.py
+++ ch4/descriptor.py
@@ -1,11 +1,11 @@
-class RevealAccess(object):
-    
+class RevealAccess:
     def __init__(self, initval=None, name='var'):
         self.val = initval
         self.name = name
         
     def __get__(self, obj, objtype):
         print("Retrieving", self.name)
+        return self.val
     
     def __set__(self, obj, val):
         print("Updating", self.name)
@@ -13,10 +13,13 @@ class RevealAccess(object):
         
     def __delete__(self, obj):
         print("Deleting", self.name)
+        del self.val
         
-class MyClass(object):
+class MyClass:
     x = RevealAccess(10, 'var "x"')
     y = 5
 
 m = MyClass()
+print(m.x)
+m.x = 20
 print(m.x)
