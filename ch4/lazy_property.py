d1428c8a7dcae3d172709c901e68f30fcafc7093
bdbrink <brendan.brink@outlook.com>
2024-08-06 17:31:51 -0500

    adding full implementation for decrator using lazy class

--- ch4/lazy_property.py
+++ ch4/lazy_property.py
@@ -1,3 +1,8 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
 import OpenGL.GL as gl
 from OpenGL.GL import shaders
 from functools import wraps
@@ -33,3 +38,37 @@ class ObjectUsingShaderProgram(object):
         vertex_shader = shaders.compileShader(self.vertex, gl.GL_VERTEX_SHADER)
         fragment_shader = shaders.compileShader(self.fragment, gl.GL_FRAGMENT_SHADER)
         return shaders.compileProgram(vertex_shader, fragment_shader)
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+
+    obj = ObjectUsingShaderProgram()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glUseProgram(obj.shader_program)
+
+        glBegin(GL_TRIANGLES)
+        glVertex3f(-1, -1, 0)
+        glVertex3f(1, -1, 0)
+        glVertex3f(0, 1, 0)
+        glEnd()
+
+        glUseProgram(0)
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+if __name__ == "__main__":
+    main()
