f76d79dddfc380a9664a4cdfebe159c7996bee63
bdbrink <brendan.brink@outlook.com>
2024-08-08 17:47:58 -0500

    refactor rectangle class

--- ch4/rectangle.py
+++ ch4/rectangle.py
@@ -4,33 +4,22 @@ class Rectangle:
         self.x1, self.y1 = x1, y1
         self.x2, self.y2 = x2, y2
 
-    def _width_get(self):
+    @property
+    def width(self):
+        """rectangle width measured from left"""
         return self.x2 - self.x1
     
-    def _width_set(self, value):
+    @width.setter
+    def width(self, value):
         self.x2 = self.x1 + value
         
-    def _height_get(self):
+    @property
+    def height(self):
         return self.y2 - self.y1
     
-    def _height_set(self, value):
+    @height.setter
+    def height(self, value):
         self.y2 = self.y1 + value
-        
-    width = property(
-        _width_get, _width_set,
-        doc="rectangle width measured from left"
-    )
-
-    height= property(
-        _height_get, _height_set,
-        doc="rectangle height measured from top"
-    )
-
-    def __repr__(self):
-        return "{}({}, {}, {})".format(
-            self.__class__.__name__,
-            self.x1, self.y1, self.x2, self.y2
-        )
 
 rectangle = Rectangle(10, 10, 25, 34)
 print(f"the width is: {rectangle.width} \nthe width is: {rectangle.height}")
