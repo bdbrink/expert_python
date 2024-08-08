928c2164c2e1bc20cb729afe85273a54b1954851
bdbrink <brendan.brink@outlook.com>
2024-08-08 18:32:34 -0500

    add more protocols

--- ch4/dunder_methods.py
+++ ch4/dunder_methods.py
@@ -26,27 +26,54 @@ class Book:
         else:
             raise TypeError("Unsupported operand type for +")
 
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    # Container protocol
+    def __contains__(self, item):
+        return item in self.books
+
+    def __len__(self):
+        return len(self.books)
+
+    # Callable protocol
+    def __call__(self, author):
+        return [book for book in self.books if book.author == author]
+
+    # Iterable protocol
+    def __iter__(self):
+        return iter(self.books)
+
+    # Indexing (part of container protocol)
+    def __getitem__(self, index):
+        return self.books[index]
+
 # Create some Book objects
 book1 = Book("Python Crash Course", "Eric Matthes", 544)
 book2 = Book("Fluent Python", "Luciano Ramalho", 792)
-book3 = Book("Python Crash Course", "Eric Matthes", 544)
+book3 = Book("Deep Learning", "Ian Goodfellow", 775)
 
-# Test __str__
-print(book1)
+# Create a Library
+library = Library()
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
 
-# Test __len__
-print(len(book1))
+# Test container protocol
+print(book1 in library)  # True
+print(len(library))  # 3
 
-# Test __eq__
-print(book1 == book2)
-print(book1 == book3)
+# Test callable protocol
+eric_books = library("Eric Matthes")
+print(eric_books)  # [Python Crash Course by Eric Matthes]
 
-# Test __lt__
-print(book1 < book2)
+# Test iterable protocol
+for book in library:
+    print(book.title)
 
-# Test __add__
-print(book1 + book2)
-print(book1 + 100)
-
-# This will raise a TypeError
-# print(book1 + "100")
+# Test indexing
+print(library[1])  # Fluent Python by Luciano Ramalho
