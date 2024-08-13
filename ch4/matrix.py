7cd8508bfeaef915f82d0dc14d4fb1b54b6b5996
bdbrink <brendan.brink@outlook.com>
2024-08-13 17:32:26 -0500

    fix mul method in matrix

--- ch4/matrix.py
+++ ch4/matrix.py
@@ -35,3 +35,12 @@ class Matrix:
             raise ValueError(
                 "Matrix dimensions don't match"
             )
+        
+        rows = [[0 for _ in other.rows[0]] for _ in self.rows]
+        
+        for i in range(len(self.rows)):
+            for j in range(len(other.rows[0])):
+                for k in range(len(other.rows)):
+                    rows[i][j] += self.rows[i][k] * other.rows[k][j]
+                    
+        return Matrix(rows)
