f7c8ce4ef98c5314f563cae34dc8a47f6d1726f0
bdbrink <brendan.brink@outlook.com>
2024-08-22 17:41:57 -0500

    adding partials for exponents

--- ch4/lambda_math.py
+++ ch4/lambda_math.py
@@ -1,5 +1,10 @@
 from functools import reduce
+from functools import partial
 
 print(reduce(lambda a,b: a+b, [2,2]))
 print(reduce(lambda a,b: a+b, [2,2,2]))
 print(reduce(lambda a,b: a+b, range(100)))
+
+power_of_2 = partial(pow, 2)
+print(power_of_2(2))
+print(power_of_2(5))
