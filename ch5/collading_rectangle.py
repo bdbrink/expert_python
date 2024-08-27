b020bbcf7e0a3e30e9bfabc6f823dc6f76250fe9
bdbrink <brendan.brink@outlook.com>
2024-08-27 17:24:35 -0500

    adding function to find collosions

--- ch5/collading_rectangle.py
+++ ch5/collading_rectangle.py
@@ -1,3 +1,4 @@
+import itertools
 
 def rects_collide(rect1, rect2):
     """ checks collision between rectangles
@@ -9,3 +10,14 @@ def rects_collide(rect1, rect2):
         rect1.y1 < rect2.y2 and
         rect1.y2 > rect2.y1
     )
+
+def find_collisions(objects):
+    return [
+        (item1, item2)
+        for item1, item2
+        in itertools.combinations(objects, 2)
+        if rects_collide(
+            item1.bounding_box,
+            item2.bounding_box
+        )
+    ]
