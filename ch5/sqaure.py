70a06f7da271196358f8665d6097dce0283b672c
bdbrink <brendan.brink@outlook.com>
2024-08-28 17:25:48 -0500

    adding circle and box properties

--- ch5/sqaure.py
+++ ch5/sqaure.py
@@ -30,3 +30,26 @@ class Rect:
             self.x + self.width,
             self.y + self.height
         )
+
+@dataclass
+class Circle:
+    x: float
+    y: float
+    radius: float
+    
+    @property
+    def bounding_box(self):
+        return Box(
+            self.x - self.radius,
+            self.y - self.radius,
+            self.x + self.radius,
+            self.y + self.radius
+        )
+
+@dataclass
+class Box:
+    x1: float
+    y1: float
+    x2: float
+    y2: float
+
