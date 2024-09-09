55ccbd1155c5aca81b614ca0a524c20861f5b2f3
bdbrink <brendan.brink@outlook.com>
2024-09-08 19:42:39 -0500

    adding better solution to check for sequence

--- leetcode/string_in_string.py
+++ leetcode/string_in_string.py
@@ -10,3 +10,17 @@ class Solution:
             return True
         else:
             return False
+
+class SolutionBetter:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # Pointers for both strings
+        s_pointer, t_pointer = 0, 0
+        
+        # Loop through t while checking for subsequence in s
+        while s_pointer < len(s) and t_pointer < len(t):
+            if s[s_pointer] == t[t_pointer]:
+                s_pointer += 1
+            t_pointer += 1
+        
+        # If we have matched all characters in s, return True
+        return s_pointer == len(s)
