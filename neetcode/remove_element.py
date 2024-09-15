57e1adb5c7fb64b9a690dd8521e6411d8abd1b81
bdbrink <brendan.brink@outlook.com>
2024-09-15 12:50:33 -0500

    adding better solution

--- neetcode/remove_element.py
+++ neetcode/remove_element.py
@@ -1,12 +1,18 @@
 from typing import List
 
 def remove_element(arr: List[int], element: int) -> List[int]:
+    ### better solution
+    # arr_clone = arr.copy()
+    # arr_clone.remove(element)
+    # return arr_clone
+    
     new_list = list(arr)
     for x,y in enumerate(arr):
         if y == element:
             new_list.remove(element)
 
     return new_list
+
 
 # do not modify below this line
 arr = [1, 3, 5, 7, 9]
