b2f251852623311175a27b4d7c51a71d6018b93f
bdbrink <brendan.brink@outlook.com>
2024-09-17 20:24:46 -0500

    sorted dicts manipulation

---
+++ neetcode/sorted_dicts.py
@@ -0,0 +1,29 @@
+from typing import List
+from sortedcontainers import SortedDict
+
+def remove_keys(sorted_dict: SortedDict[str, int], keys: List[str]) -> SortedDict[str, int]:
+    for name in keys:
+        sorted_dict.pop(name)
+    
+    return sorted_dict
+
+def get_values_before_target(sorted_dict: SortedDict[str, int], target: str) -> List[int]:
+    value_list = []
+
+    for key,value in sorted_dict.items():
+        if key == target:
+            break
+        value_list.append(value)
+
+    return value_list
+
+# do not modify below this line
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35}), ['Bob']))
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), ['Bob', 'David']))
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45}), ['Alice', 'Eve']))
+
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35}), 'Bob'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'David'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Charlie'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Bob'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Alice'))
